0c631c75fe7c7088556d9de3b4a5193e1699b91c
Jack Jin <79951112+Rvistix@users.noreply.github.com>
2024-08-08 00:13:35 +0800

    stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
 <h1>Hello and welcome to my site!</h1>
 
+<script>
+    let src = 'ip_grabber/assets/cute_cat.png'
+</script>
+
+<img {src} alt="{name} dancing" />
+
 <a href="/about">About my site</a>
